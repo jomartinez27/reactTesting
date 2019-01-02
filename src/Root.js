9317ca25997e05b15ca89ff9f2efbda46ea7f4cf
Jose Martinez <josmmartinez94@gmail.com>
2019-01-02 13:46:09 -0800

    Add applyMiddleware, add axios action

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
 export default ({ children, initialState = {} }) => {
   const store = createStore(
-    reducers, 
+    reducers,
     initialState,
-    applyMiddleware(reduxPromise));
+    applyMiddleware(reduxPromise)
+  );
 
   return (
     <Provider store={store}>
