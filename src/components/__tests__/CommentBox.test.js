654b6b949d3ee76c22aa125928beb69335b696cd
Jose Martinez <josmmartinez94@gmail.com>
2019-01-02 12:04:59 -0800

    Add describe block for text area CommentBox.test.js

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,10 +16,21 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment'}
-  });
-  wrapped.update();
+describe('Text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment'}
+    });
+    wrapped.update();
+  })
+
+  it('has a text area that users can type in', () => {
   expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 });
+
+  it('clears text area after submit', () => {
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+})
+})
