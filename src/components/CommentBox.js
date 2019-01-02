82f44ddaa2071972c5b726aefc758b306572e8e3
Jose Martinez <josmmartinez94@gmail.com>
2019-01-02 10:30:44 -0800

    Change CommentBox to pure component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React from 'react'
 
-export default () => (
-  <div>Comment box</div>
-)
+class CommentBox extends React.Component {
+  state = { comment: '' };
+
+  handleChange = (e) => {
+    this.setState({ comment: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    
+    this.setState({ comment: ''});
+  };
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange}/>
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default CommentBox;
