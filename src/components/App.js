e0c7191732461afb30f36406d15d563a493b219f
Jose Martinez <josmmartinez94@gmail.com>
2019-01-02 07:49:43 -0800

    Setup project structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
 
 class App extends React.Component {
   render () {
     return (
       <div>
-        App Component
+        <CommentBox />
+        <CommentList />
       </div>
     )
   }
