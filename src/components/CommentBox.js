17cb46e5e942f0662dcc61de74179432333a8f93
Jose Martinez <josmmartinez94@gmail.com>
2019-01-03 07:56:05 -0800

    Add HOC for CommentBox component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import * as actions from 'actions';
+import requireAuth from 'components/requireAuth';
 
 class CommentBox extends React.Component {
   state = { comment: '' };
@@ -12,7 +13,6 @@ class CommentBox extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.saveComment(this.state.comment)
-
     this.setState({ comment: ''});
   };
 
@@ -32,4 +32,4 @@ class CommentBox extends React.Component {
   }
 }
 
-export default connect(null, actions)(CommentBox);
+export default connect(null, actions)(requireAuth(CommentBox));
